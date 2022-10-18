6bff4dcd165392fadc573996271b3afdbd7efd36
Nicolas Pantoja <nicolaspantoja@hotmail.com>
2022-10-18 02:43:44 -0400

    Configurando o crud e integrand jwt para geração de token para o usuario

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -1,36 +1,72 @@
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { json } = require("body-parser");
 
 module.exports = {
   async index(req, res) {
-    const users = await User.findAll();
+    const user = await User.findAll({
+      attributes: ["id", "name", "email"],
+      order: [["id", "DESC"]],
+    });
 
-    return res.json(users);
+    console.log(user);
+
+    return user;
   },
 
   async store(req, res) {
-    const { name, email, senha } = req.body;
+    let dados = req.body;
+    console.log(dados);
+    dados.senha = await bcrypt.hash(dados.senha, 8);
 
-    const user = await User.create({ name, email, senha });
-
-    return res.json(user);
+    await User.create(dados)
+      .then(() => {
+        return res.json({
+          erro: false,
+          mensagem: "Usuário cadastrado com sucesso!",
+        });
+      })
+      .catch(() => {
+        return res.status(400).json({
+          erro: true,
+          mensagem: "Erro: Usuário não cadastrado com sucesso!",
+        });
+      });
   },
 
-  async logar(req, res) {
-    const dados = req.body;
-    console.log(dados);
+  async login(req, res) {
+    console.log(req.body);
     const user = await User.findOne({
       attributes: ["id", "name", "email", "senha"],
       where: {
-        email: dados.email,
+        email: req.body.email,
       },
     });
 
     if (user === null) {
       return res.status(400).json({
         erro: true,
-        mensagem:
-          "Erro: Usuário ou a senha incorreta! Nenhum usuário com este e-mail",
+        mensagem: "Erro: Usuário ou senha incorreto",
       });
     }
+
+    if (!bcrypt.compare(req.body.senha, user.senha)) {
+      return res.status(400).json({
+        erro: true,
+        mensagem: "Erro: Usuário ou a senha incorreta! Senha incorreta!",
+      });
+    }
+
+    let token = jwt.sign({ id: user.id }, "D62ST92Y7A6V7K5C6W9ZU6W8KS3", {
+      expiresIn: 600, //10 min
+      // expiresIn: 60, //1 min
+      // expiresIn: "7d", // 7 dia
+    });
+    return res.json({
+      erro: false,
+      mensagem: "Login realizado com sucesso",
+      token,
+    });
   },
 };
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
+const { isFather } = require("./middlewares/auth");
 
 const UserController = require("./controller/UserController");
 const e = require("express");
+const User = require("./models/User");
 
 const routes = express.Router();
 
@@ -14,9 +16,25 @@ routes.get("/login", (req, res) => {
 routes.get("/cadastro", (req, res) => {
   res.render("cadastrar.html");
 });
+routes.get("/", isFather, async (req, res) => {
+  await UserController.index()
+    .then((user) => {
+      return res.json({
+        erro: false,
+        user,
+        id_usuario_logado: req.userId,
+      });
+    })
+    .catch(() => {
+      return res.status(400).json({
+        erro: true,
+        mensagem: "Erro: Nenhum usuário encontrado!",
+      });
+    });
+});
 
 routes.post("/cadastrar", UserController.store);
 routes.get("/users", UserController.index);
-routes.post("/logar", UserController.logar);
+routes.post("/logar", UserController.login);
 
 module.exports = routes;
